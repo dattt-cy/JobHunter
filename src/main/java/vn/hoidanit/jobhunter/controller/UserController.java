a34861ca3c333fb19b9a42049c269fa542dfaf94
dattt-cy <victomblack1602@gmail.com>
2025-12-26 17:00:29 +0700

    first commit

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -0,0 +1,39 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user")
+    public User createNewUser(@RequestBody User postManUser) {
+
+        // User user = new User();
+        // user.setEmail("hoidanit@gmail.com");
+        // user.setName("Eric");
+        // user.setPassword("123456");
+
+        User ericUser = this.userService.handleCreateUser(postManUser);
+
+        return ericUser;
+    }
+
+    @DeleteMapping("/user/{id}")
+    public String deleteUser(@PathVariable("id") long id) {
+        this.userService.handleDeleteUser(id);
+        return "ericUser";
+    }
+}
