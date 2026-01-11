ef4581eea47390619f69ea07f8f2b479e555d67b
dattt-cy <victomblack1602@gmail.com>
2026-01-11 13:03:45 +0700

    "them api crud cho subscriber"

--- src/main/java/vn/hoidanit/jobhunter/service/SkillService.java
+++ src/main/java/vn/hoidanit/jobhunter/service/SkillService.java
@@ -44,6 +44,7 @@ public class SkillService {
         Optional<Skill> skillOptional = this.skillRepository.findById(id);
         Skill currentSkill = skillOptional.get();
         currentSkill.getJobs().forEach(job -> job.getSkills().remove(currentSkill));
+        currentSkill.getSubscribers().forEach(subs -> subs.getSkills().remove(currentSkill));
 
         // delete skill
         this.skillRepository.delete(currentSkill);
