dcb8cd0134f5aa18ba5ae57681b696c676ad1c3f
dattt-cy <victomblack1602@gmail.com>
2026-01-11 13:40:35 +0700

    "cau hinh send Email voi template engine"

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/EmailController.java
@@ -0,0 +1,30 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.service.EmailService;
+import vn.hoidanit.jobhunter.util.annotation.ApiMessage;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmailController {
+
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @GetMapping("/email")
+    @ApiMessage("Send simple email")
+    public String sendSimpleEmail() {
+        // this.emailService.sendSimpleEmail();
+        // this.emailService.sendEmailSync("victomblack1710@gmail.com", "Test send
+        // email", "<h1> <b>Hello </b> </h1>",
+        // false, true);
+        this.emailService.sendEmailFromTemplateSync("victomblack1710@gmail.com", "Test send email", "job");
+        return "ok";
+    }
+}
